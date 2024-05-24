3e72a42452414ee2fddbdb16f021c7cbfbe25dae
C7197Recep <yalcinr896@gmail.com>
2024-05-25 00:44:15 +0300

    ECW-2024 feat: loopback backend created

---
+++ backend/src/controllers/index.ts
@@ -0,0 +1,3 @@
+export * from './ping.controller';
+export * from './e-commerce.controller';
+export * from './customer.controller';
---
+++ backend/src/models/customer.model.ts
@@ -0,0 +1,63 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model
+
+/* BU KISMI EKLEMEZSEK MONGODB TARAFINDA TABLE Customers SEKLINDE OLUR. */
+({
+  settings: {
+    mongodb: {
+      collection: 'customers',
+    }
+  },
+}) 
+/* BU KISMI EKLEMEZSEK MONGODB TARAFINDA TABLE Customers SEKLINDE OLUR. */
+
+export class Customer extends Entity {
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Ad: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Soyad: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Firma: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  GSM: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Adres: string;
+
+  @property({
+    type: 'number',
+    id: true,
+    generated: true,
+  })
+  ID?: number;
+
+
+  constructor(data?: Partial<Customer>) {
+    super(data);
+  }
+}
+
+export interface CustomerRelations {
+  // describe navigational properties here
+}
+
+export type CustomerWithRelations = Customer & CustomerRelations;
