4368ee3764c1a7af5039dc42f9508817144bfa32
C7197Recep <yalcinr896@gmail.com>
2024-06-05 21:38:14 +0300

    ECW 2024 fix: page reduction

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,17 @@
+import React from 'react';
+
 import './App.css';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import React from 'react';
+
 import { Provider } from 'react-redux';
 import store from './redux/store';
 
 import Home from './pages/Home';
 import About from './pages/About';
 import Customers from './pages/Customers';
-import Register from './pages/Register';
-
-import Customers2 from './pages/Customers2';
 import CustomerForm from './pages/CustomerForm';
+import Register from './pages/Register';
 
 import Header from './components/Header';
 import Footer from './components/Footer';
@@ -29,12 +29,12 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/About' component={About} />
 
-          <Route path='/Customers' component={Customers} />
-          <Route path='/Register' component={Register} />
 
-          <Route path='/Customers2' component={Customers2} />
+          <Route path='/Customers' component={Customers} />
           <Route path="/add-customer" component={CustomerForm} />
           <Route path="/edit-customer/:id" component={CustomerForm} />
+
+          <Route path='/Register' component={Register} />
 
         </Switch>
         <Footer />
