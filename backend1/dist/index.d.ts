8f60442983e46e3ba680749ba93b72700bb554c6
C7197-Recep <75995945+C7197-Recep@users.noreply.github.com>
2024-06-09 00:47:16 +0300

    Delete backend1 directory

--- backend1/dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { ApplicationConfig, ECommerceSampleApplication } from './application';
-export * from './application';
-export declare function main(options?: ApplicationConfig): Promise<ECommerceSampleApplication>;
