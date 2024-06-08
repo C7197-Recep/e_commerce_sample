c468db03fa7b650270ba42ce31653779d0614f44
C7197Recep <yalcinr896@gmail.com>
2024-06-09 02:36:59 +0300

    gitignore

---
+++ backend/src/datasources/mongodb.datasource.ts
@@ -0,0 +1,55 @@
+import {inject, lifeCycleObserver, LifeCycleObserver} from '@loopback/core';
+import {juggler} from '@loopback/repository';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+/* data klasörü içinde bir FAKE DB oluşturup ona bağlayabiliriz.*/
+// const config = {
+//   name: 'db',
+//   connector: 'memory',
+//   localStorage: '',
+//   file: './data/db.json'
+// };
+
+/* ONLINE DB'ye bağlayabiliriz. 
+Buradaki "host" bilgisini veritabanına erişim sağladığı
+için Github'a yüklemeden önce .env vb. yöntemle
+emniyete almayı unutma.*/
+const config = {
+  name: 'db',
+  connector: 'mongodb',
+  host: process.env.MONGODB_HOST,
+  database: 'e-commerce-sample',
+  protocol: 'mongodb+srv',
+};
+
+/* LOCAL DB'ye bağlayabiliriz. */
+// const config = {
+//   name: 'db',
+//   connector: 'mongodb',
+//   url: '',
+//   host: 'localhost',
+//   port: 27017,
+//   user: '',
+//   password: '',
+//   database: 'testdb',
+// };                    
+
+// Observe application's life cycle to disconnect the datasource when
+// application is stopped. This allows the application to be shut down
+// gracefully. The `stop()` method is inherited from `juggler.DataSource`.
+// Learn more at https://loopback.io/doc/en/lb4/Life-cycle.html
+@lifeCycleObserver('datasource')
+export class MongodbDataSource extends juggler.DataSource
+  implements LifeCycleObserver {
+  static dataSourceName = 'mongodb';
+  static readonly defaultConfig = config;
+
+  constructor(
+    @inject('datasources.config.mongodb', {optional: true})
+    dsConfig: object = config,
+  ) {
+    super(dsConfig);
+  }
+}
---
+++ backend/src/models/customer.model.ts
@@ -0,0 +1,61 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model
+
+/* BU KISMI EKLEMEZSEK MONGODB TARAFINDA TABLE Customers SEKLINDE OLUR. */
+({
+  settings: {
+    mongodb: {
+      collection: 'customers',
+    }
+  },
+}) 
+/* BU KISMI EKLEMEZSEK MONGODB TARAFINDA TABLE Customers SEKLINDE OLUR. */
+
+export class Customer extends Entity {
+  
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Ad: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Soyad: string;
+
+  @property({
+    type: 'string',
+  })
+  Firma?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  GSM: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  Adres: string;
+
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  constructor(data?: Partial<Customer>) {
+    super(data);
+  }
+}
+
+export interface CustomerRelations {
+  // describe navigational properties here
+}
+
+export type CustomerWithRelations = Customer & CustomerRelations;
